7ebba3f13336017e185374321fb944524e8230c7
TsukasaOrita <Tsukasa680@gmail.com>
2021-03-13 14:08:22 +0000

    [start]6-4

--- meshiterro/config/initializers/application_controller_renderer.rb
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'd816e5b5be57f81b50a3c73fb20e8bbcf9bff7b712aad873db5ce5f259f05c7bee998ad337a301810008ab91eb195a66fa8befba1b52d171a2c32eb5c93ca77a'
